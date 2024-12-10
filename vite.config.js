d96a80bf853a862fb2f08fbe0045f18fe3e749f4
Yuan <badshah.ali8704@gmail.com>
2024-12-10 18:10:39 +0530

    innitialize commits

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Nvc.in/', // Update this to match your GitHub repo name
+  base: '/nvc-web/',  // Updated to match repository name
   plugins: [react()],
   build: {
     minify: 'terser',

b1c06ad229492a4b1708ae82ef86d1a299a43f04
Yuan <badshah.ali8704@gmail.com>
2024-12-10 18:04:42 +0530

    Updates

---
+++ assets/utilities-DpLOzXBt.js
@@ -0,0 +1,1 @@
+import{r,g as s}from"./vendor--DjWAcXK.js";var a=r();const o=s(a);export{o as R,a as r};
---
+++ serviceWorker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'nvc-web-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/assets/index.css',
+  '/assets/index.js',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          (response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+  );
+});
